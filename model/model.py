39f1f24b2bebcae70d95ed048f61b014080dfc77
Federico <frossimiglio@gmail.com>
2025-11-24 15:24:44 +0100

    Lab09,scritto le query, implementato il metodo loadrelazioni(ORM),implementato il metedo genera_pacchetto e l'algoritmo ricorsivo, programma completato

--- model/model.py
+++ model/model.py
@@ -12,8 +12,9 @@ class Model:
         self._pacchetto_ottimo = []
         self._valore_ottimo: int = -1
         self._costo = 0
+        self._tour_candidati = [] #lista dove inserisco solo i tour che servono in base alla regione selezionata
 
-        # TODO: Aggiungere eventuali altri attributi
+
 
         # Caricamento
         self.load_tour()
@@ -43,13 +44,13 @@ class Model:
         lista_relazione = TourDAO.get_tour_attrazioni() # relazione N:N [id_tour | id_attrazione ]
 
         for rel in lista_relazione:
-            self.tour_map[rel['id_tour']].attrazione.add(rel['id_attrazione'])
+            self.tour_map[rel['id_tour']].attrazioni.add(rel['id_attrazione'])
             self.attrazioni_map[rel['id_attrazione']].tour.add(rel['id_tour'])
 
 
 
 
-        # TODO
+
 
     def genera_pacchetto(self, id_regione: str, max_giorni: int = None, max_budget: float = None):
         """
@@ -65,12 +66,85 @@ class Model:
         self._pacchetto_ottimo = []
         self._costo = 0
         self._valore_ottimo = -1
+        self._tour_candidati = []
 
-        # TODO
+        for t in self.tour_map.values(): #for per prendere solamente i tour della regione scelta(minimizzare le operazioni che il calcolaatore deve eseguire)
+            if t.id_regione == id_regione:
+                self._tour_candidati.append(t)
+        self._ricorsione(0,[],0,0,0,set(),max_giorni, max_budget) #chiamo la ricorsione
+
+
 
         return self._pacchetto_ottimo, self._costo, self._valore_ottimo
 
-    def _ricorsione(self, start_index: int, pacchetto_parziale: list, durata_corrente: int, costo_corrente: float, valore_corrente: int, attrazioni_usate: set):
-        """ Algoritmo di ricorsione che deve trovare il pacchetto che massimizza il valore culturale"""
+    def _ricorsione(self, start_index: int, pacchetto_parziale: list, durata_corrente: int,
+                    costo_corrente: float, valore_corrente: int, attrazioni_usate: set,
+                    max_giorni: int, max_budget: float):
 
-        # TODO: è possibile cambiare i parametri formali della funzione se ritenuto opportuno
+        # verifica se la soluzione attuale è migliore del massimo trovato finora
+        if valore_corrente > self._valore_ottimo:
+            self._valore_ottimo = valore_corrente
+            self._pacchetto_ottimo = pacchetto_parziale.copy()  # faccio una copia della lista per salvarla
+            self._costo = costo_corrente
+
+        # caso base: interrompo se ho esaminato tutti i tour disponibili
+        if start_index >= len(self._tour_candidati):
+            return
+
+        # recupero il tour corrente dalla lista filtrata
+        tour_corrente = self._tour_candidati[start_index]
+
+        # ramo 1: provo ad aggiungere il tour (take)
+
+        # verifica vincolo budget
+        budget_ok = True
+        if max_budget is not None and (costo_corrente + tour_corrente.costo) > max_budget:
+            budget_ok = False
+
+        # verifica vincolo durata
+        durata_ok = True
+        if max_giorni is not None and (durata_corrente + tour_corrente.durata_giorni) > max_giorni:
+            durata_ok = False
+
+        # verifica vincolo attrazioni duplicate (isdisjoint ritorna true se non ci sono elementi comuni)
+        attrazioni_ok = attrazioni_usate.isdisjoint(tour_corrente.attrazioni)
+
+        # se tutti i vincoli sono rispettati, procedo con l'aggiunta
+        if budget_ok and durata_ok and attrazioni_ok:
+
+            # calcolo il valore culturale sommando quello delle singole attrazioni
+            valore_tour_curr = 0
+            for id_attr in tour_corrente.attrazioni:
+                valore_tour_curr += self.attrazioni_map[id_attr].valore_culturale
+
+            # aggiungo il tour e le sue attrazioni alle strutture dati parziali
+            pacchetto_parziale.append(tour_corrente)
+            for att in tour_corrente.attrazioni:
+                attrazioni_usate.add(att)
+
+            # chiamata ricorsiva aggiornando i contatori (costo, durata, valore)
+            self._ricorsione(
+                start_index + 1,
+                pacchetto_parziale,
+                durata_corrente + tour_corrente.durata_giorni,
+                costo_corrente + tour_corrente.costo,
+                valore_corrente + valore_tour_curr,
+                attrazioni_usate,
+                max_giorni, max_budget
+            )
+
+            # backtracking: rimuovo l'ultimo tour e le sue attrazioni per ripristinare lo stato
+            pacchetto_parziale.pop()
+            for att in tour_corrente.attrazioni:
+                attrazioni_usate.remove(att)
+
+        # ramo 2: salto il tour corrente e passo al successivo
+        self._ricorsione(
+            start_index + 1,
+            pacchetto_parziale,
+            durata_corrente,
+            costo_corrente,
+            valore_corrente,
+            attrazioni_usate,
+            max_giorni, max_budget
+        )
